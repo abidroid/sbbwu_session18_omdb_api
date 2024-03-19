6aba7068e1bb6ade79fc2f2939003a814b7b4a32
Abid <abidroid1@gmail.com>
2024-03-19 16:20:29 +0500

    model created

---
+++ lib/screens/movie_search_screen.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class MovieSearchScreen extends StatefulWidget {
+  const MovieSearchScreen({super.key});
+
+  @override
+  State<MovieSearchScreen> createState() => _MovieSearchScreenState();
+}
+
+class _MovieSearchScreenState extends State<MovieSearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
